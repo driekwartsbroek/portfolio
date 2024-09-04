11929628b51ec55255cebcfeac96390e3b731da0
Bert Selleslagh <SELLESLAGHBERT@GMAIL.COM>
2024-09-04 19:37:30 +0200

    Ensure workExperiences is a .ts file and update imports

---
+++ src/data/workExperiences.ts
@@ -0,0 +1,53 @@
+export const workExperiences = [
+  {
+    company: "Solidaris",
+    projects: [
+      {
+        id: "1",
+        title: "e-Mut",
+        description: "Description of Project 1",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+      {
+        id: "2",
+        title: "ScanSequel",
+        description: "Description of Project 2",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+      {
+        id: "3",
+        title: "Project 3",
+        description: "Description of Project 3",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+    ],
+  },
+  {
+    company: "Tobania",
+    projects: [
+      {
+        id: "4",
+        title: "Project 4",
+        description: "Description of Project 4",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+      {
+        id: "5",
+        title: "Project 5",
+        description: "Description of Project 5",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+    ],
+  },
+  {
+    company: "Colruyt",
+    projects: [
+      {
+        id: "6",
+        title: "Project 6",
+        description: "Description of Project 6",
+        imageUrl: "https://placehold.co/600x400/png",
+      },
+    ],
+  },
+];
